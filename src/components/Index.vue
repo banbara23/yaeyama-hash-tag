6cd779e175bcecc889424a33547226ecc74df10e
ikemura <banbara.work@gmail.com>
2017-06-09 10:38:31 +0900

    スタイル無し初期実装完了

---
+++ src/components/Index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="index">
+    <h1>やえやまハッシュタグ</h1>
+    <h2>八重山のTwitterハッシュタグが大集合</h2>
+    <div class="timeline">
+      <time-line :query='"%E5%85%AB%E9%87%8D%E5%B1%B1"'
+                 :id='"872701594977976320"'
+                 :word='"八重山"' />
+      <!--<time-line :query='"%E7%AB%B9%E5%AF%8C%E7%94%BA"' :id='"872720186729680896"' :word='"竹富町"' />-->
+      <time-line :query='"%E7%9F%B3%E5%9E%A3%E5%B3%B6"'
+                 :id='"872719272245567492"'
+                 :word='"石垣島"' />
+      <time-line :query='"%E8%A5%BF%E8%A1%A8%E5%B3%B6"'
+                 :id='"872720987854225409"'
+                 :word='"西表島"' />
+      <time-line :query='"%E7%AB%B9%E5%AF%8C%E5%B3%B6"'
+                 :id='"872721296886448128"'
+                 :word='"竹富島"' />
+      <time-line :query='"%E5%B0%8F%E6%B5%9C%E5%B3%B6"'
+                 :id='"872723027510779904"'
+                 :word='"小浜島"' />
+      <time-line :query='"%E9%B3%A9%E9%96%93%E5%B3%B6"'
+                 :id='"872723438430855168"'
+                 :word='"鳩間島"' />
+      <time-line :query='"%E9%BB%92%E5%B3%B6"'
+                 :id='"872723666856890368"'
+                 :word='"黒島"' />
+      <time-line :query='"%E6%B3%A2%E7%85%A7%E9%96%93%E5%B3%B6"'
+                 :id='"872723666856890368"'
+                 :word='"波照間島"' />
+    </div>
+  </div>
+</template>
+
+<script>
+import timeLine from './Timeline.vue'
+export default {
+  name: 'index',
+  components: {
+    timeLine
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+div#timeline {
+  display: inline-block;
+}
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="timeline">
+    <a class="twitter-timeline"
+       width='250'
+       :href="`https://twitter.com/hashtag/${query}`"
+       :data-widget-id="id">#{{word}} のツイート</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+  props: ['query','id', 'word'],
+  mounted: function () {
+    (!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + "://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } }(document, "script", "twitter-wjs"))
+  }
+}
+</script>
+
+<style>
+
+</style>
