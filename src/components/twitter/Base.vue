ce7804c6031dfef00831255ef37a72e5d000b6be
ikemura <banbara.work@gmail.com>
2017-06-16 14:01:04 +0900

    実験でドロップダウンリストを表示

--- src/components/twitter/Base.vue
+++ src/components/twitter/Base.vue
@@ -1,5 +1,23 @@
 <template>
   <div id="timeline">
+    <!-- Dropdown Trigger -->
+    <a class='dropdown-button btn'
+       href='#'
+       data-activates='dropdown1'>タグを選択<i class="material-icons right">arrow_drop_down</i></a>
+  
+    <!-- Dropdown Structure -->
+    <ul id='dropdown1'
+        class='dropdown-content'>
+      <li><a href="#!">#八重山</a></li>
+      <li><a href="#!">#石垣島</a></li>
+      <li><a href="#!">#西表島</a></li>
+      <li><a href="#!">#竹富島</a></li>
+      <li><a href="#!">#小浜島</a></li>
+      <li><a href="#!">#黒島</a></li>
+      <li><a href="#!">#鳩間島</a></li>
+      <li><a href="#!">#波照間島</a></li>
+      <li><a href="#!">すべて</a></li>
+    </ul>
     <all />
   </div>
 </template>
@@ -11,7 +29,10 @@ export default {
   components: {
     all
   },
-  props: ['query', 'id', 'word']
+  props: ['query', 'id', 'word'],
+  mounted() {
+    $(".dropdown-button").dropdown();
+  }
 }
 </script>
 
