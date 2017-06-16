f2fd3486cd624bea6991d47f07715deac09a1811
ikemura <banbara.work@gmail.com>
2017-06-16 15:03:21 +0900

    Navにsvgアイコン表示

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,26 @@
 <template>
   <nav class="nav-extended orange darken-1">
     <div class="nav-wrapper">
-      <router-link to="/" class="brand-logo center">やえやまハッシュタグ</router-link>
+      <router-link to="/"
+                   class="brand-logo center">やえやまハッシュタグ</router-link>
     </div>
     <ul class="left">
-      <li><router-link to="/twitter">Twiter</router-link></li>
-      <li><router-link to="/instagram">Instagram</router-link></li>
+      <li>
+        <router-link to="/twitter"><img src="../assets/Twitter_Logo_White_On_Image.svg">Twiter</router-link>
+      </li>
+      <li>
+        <router-link to="/instagram"><img src="../assets/instagram_icon.svg">Instagram</router-link>
+      </li>
     </ul>
+  
   </nav>
 </template>
+
+<style scoped>
+ul {
+  width: 100%
+}
+ul li {
+  width: 50%;
+}
+</style>
