18681c68774a36e27b4f416889c5d2ec20668881
ikemura <banbara.work@gmail.com>
2017-06-16 14:00:25 +0900

    Navbarリファクタ

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,9 @@
 <template>
   <nav class="nav-extended orange darken-1">
     <div class="nav-wrapper">
-      
-      <h4 class="white-text"><a href="#">やえやまハッシュタグ</a></h4>
-      <p>八重山のハッシュタグが大集合</p>
+      <router-link to="/" class="brand-logo center">やえやまハッシュタグ</router-link>
     </div>
     <ul class="left">
-      <!--<li　class="active"><a href="#">#八重山</a></li>
-              <li><a href="#">#石垣島</a></li>
-              <li><a href="#">#西表島</a></li>
-              <li><a href="#">#竹富島</a></li>
-              <li><a href="#">#小浜島</a></li>
-              <li><a href="#">#黒島</a></li>
-              <li><a href="#">#鳩間島</a></li>
-              <li><a href="#">#波照間島</a></li>-->
       <li><router-link to="/twitter">Twiter</router-link></li>
       <li><router-link to="/instagram">Instagram</router-link></li>
     </ul>
