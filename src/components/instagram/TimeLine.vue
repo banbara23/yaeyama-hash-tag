89f067375c9a48ae9fc3bb0df427e1dd804c0cf6
ikemura <banbara.work@gmail.com>
2017-06-22 14:00:45 +0900

    スタイル修正

--- src/components/instagram/TimeLine.vue
+++ src/components/instagram/TimeLine.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="instagram">
     <h4>{{name}}</h4>
-    <div class="col s12 m6 l4" v-for="m in media">
-      <div class="card" v-if="media">
+    <div class="col s12 m6 l4"
+         v-for="m in media">
+      <div class="card"
+           v-if="media">
         <div class="card-image waves-effect">
-          <img class="activator" :src="m.thumbnail_src">
+          <img class="activator"
+               :src="m.thumbnail_src">
         </div>
         <div class="card-reveal">
           <span class="card-title grey-text text-darken-4">
-            <i class="material-icons Large right">close</i>
-          </span>
+              <i class="material-icons Large right">close</i>
+            </span>
           <p class="left-align">{{m.data}}</p>
           <p class="left-align">{{m.caption}}</p>
-          <a id="insta-btn" class="orange lighten-5 waves-effect waves-teal btn-flat left-align" :href="`https://www.instagram.com/p/${m.code}/`" target="_blank">Instagramで見る</a>
+          <a id="insta-btn"
+             class="orange lighten-5 waves-effect waves-teal btn-flat left-align"
+             :href="`https://www.instagram.com/p/${m.code}/`"
+             target="_blank">Instagramで見る</a>
         </div>
       </div>
     </div>
@@ -32,6 +38,7 @@ export default {
 h4 {
   font-weight: normal;
 }
+
 #insta-btn {
   margin-top: 10px;
 }
