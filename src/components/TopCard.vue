92880c9b13e4261b10524b441f33ca458fd02459
ikemura <banbara.work@gmail.com>
2017-06-23 17:36:08 +0900

    トップカード表示を変更

--- src/components/TopCard.vue
+++ src/components/TopCard.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="top-card">
     <div class="card">
+      <router-link :to="{path:`${code}`}">
       <div class="card-image">
-        <img :src="url" v-if="url">
-        <div id="placeHolder" v-else="url"></div>
-        <span class="card-title">#{{title}}</span>
+        <img :src="url"
+             v-if="url">
+        <div id="placeHolder"
+             v-else="url"></div>
       </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">#{{title}}</span>
+      </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -17,7 +23,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #placeHolder {
   width: 300px;
