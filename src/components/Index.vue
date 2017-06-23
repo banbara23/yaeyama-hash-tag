dac14043243c1ebd696188190c32718ee087cecd
ikemura <banbara.work@gmail.com>
2017-06-23 14:47:36 +0900

    トップに各タグのcardを表示

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,22 +1,58 @@
 <template>
   <div class="index">
-    <p>八重山のハッシュタグが大集合</p>
-    <p>上部メニューから見たいタグを選んでください</p>
+    <vue-progress-bar />
+    <p>Instagramの八重山のハッシュタグ画像が大集合</p>
+    <div class="container">
+      <div class="row">
+        <div class="col s6 m4 l4"
+             v-for="data in result">
+          <top-card :title="data.title"
+                    :code="data.code"
+                    :url="data.url"></top-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import topCard from './TopCard.vue';
+import firebase from 'firebase'
+const db = firebase.database()
 
 export default {
   name: 'index',
   components: {
+    topCard
+  },
+  data() {
+    return {
+      result:
+      [
+        { code: 'yaeyama', title: '八重山', url: '' },
+        { code: 'ishigaki', title: '石垣島', url: '' },
+        { code: 'iriomote', title: '西表島', url: '' },
+        { code: 'taketomi', title: '竹富島', url: '' },
+        { code: 'kohama', title: '小浜島', url: '' },
+        { code: 'kuroshima', title: '黒島', url: '' },
+        { code: 'hatoma', title: '鳩間島', url: '' },
+        { code: 'hateruma', title: '波照間島', url: '' },
+        { code: 'yonaguni', title: '与那国島', url: '' }
+      ]
+    }
+  },
+  mounted() {
+    this.$Progress.start();
+  },
+  firebase() {
+    return {
+      result: {
+        source: db.ref('/top'),
+        readyCallback: function () {
+          this.$Progress.finish();
+        }
+      }
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-div.index {
-  margin-top: 20px;
-}
-</style>
---
+++ src/components/TopCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="top-card">
+    <div class="card">
+      <div class="card-image">
+        <img :src="url" v-if="url">
+        <div id="placeHolder" v-else="url"></div>
+        <span class="card-title">#{{title}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '#top-card',
+  props: ['title', 'code', 'url']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#placeHolder {
+  width: 300px;
+  height: 300px;
+  background-color: #ccc;
+}
+</style>
